b55f56bd510a48afa801db253b353ef9cc5f0c8b
Mats Byrkjeland <matsbyr@gmail.com>
2016-01-19 00:18:06 -0800

    Indent with 4 spaces instead of 2

--- client/js/search.js
+++ client/js/search.js
@@ -1,33 +1,33 @@
 Template.search.events({
-  'click tr': function() {
-    Session.set('selectedArticle', this._id);
-  },
-  'keyup [type=text]': function(event, template) {
-    Session.set('searchText', event.target.value);
-  }
+    'click tr': function() {
+        Session.set('selectedArticle', this._id);
+    },
+    'keyup [type=text]': function(event, template) {
+        Session.set('searchText', event.target.value);
+    }
 });
 
 Template.search.helpers({
-  'article': function() {
+    'article': function() {
 
-    // If search field is empty, show all
-    if (!Session.get('searchText') || !Session.get('searchText').trim()) {
-      return ArticleList.search("").fetch();
-    }
+        // If search field is empty, show all
+        if (!Session.get('searchText') || !Session.get('searchText').trim()) {
+            return ArticleList.search("").fetch();
+        }
 
-    // Array of all space-separated keywords
-    var keywords = Session.get("searchText").trim().split(" ");
+        // Array of all space-separated keywords
+        var keywords = Session.get("searchText").trim().split(" ");
 
-    // Make a search for each keyword and intersect with resultArray to find articles present in all (i.e. the union)
-    var resultArray = ArticleList.search(keywords[0].trim()).fetch();
-    for (var i = 1; i < keywords.length; i++) {
-      resultArray = intersect(resultArray, ArticleList.search(keywords[i].trim()).fetch());
+        // Make a search for each keyword and intersect with resultArray to find articles present in all
+        var resultArray = ArticleList.search(keywords[0].trim()).fetch();
+        for (var i = 1; i < keywords.length; i++) {
+            resultArray = intersect(resultArray, ArticleList.search(keywords[i].trim()).fetch());
+        }
+        return resultArray;
+    },
+    'selectedClass': function() {
+        if (Session.get('selectedArticle') === this._id) {
+            return 'selected';
+        }
     }
-    return resultArray;
-  },
-  'selectedClass': function() {
-    if (Session.get('selectedArticle') === this._id) {
-      return 'selected';
-    }
-  }
 });
--- server/methods.js
+++ server/methods.js
@@ -1,35 +1,34 @@
 if (Meteor.isServer) {
+    Meteor.methods({
+        'insertArticle': function(edition, url, pages, title, author, layout, type, tags) {
+            ArticleList.insert({
+                "edition": edition,
+                "url": url,
+                "pages": pages,
+                "title": title,
+                "author": author,
+                "layout": layout,
+                "type": type,
+                "tags": tags
+            });
+        },
+        'updateArticle': function(selectedArticle, edition, url, pages, title, author, layout, type, tags) {
+            ArticleList.update(
+                {_id: selectedArticle},
+                {
+                    "edition": edition,
+                    "url": url,
+                    "pages": pages,
+                    "title": title,
+                    "author": author,
+                    "layout": layout,
+                    "type": type,
+                    "tags": tags
+                });
 
-  Meteor.methods({
-    'insertArticle': function(edition, url, pages, title, author, layout, type, tags) {
-      ArticleList.insert({
-        "edition": edition,
-        "url": url,
-        "pages": pages,
-        "title": title,
-        "author": author,
-        "layout": layout,
-        "type": type,
-        "tags": tags
-      });
-    },
-    'updateArticle': function(selectedArticle, edition, url, pages, title, author, layout, type, tags) {
-      ArticleList.update(
-        {_id: selectedArticle},
-        {
-          "edition": edition,
-          "url": url,
-          "pages": pages,
-          "title": title,
-          "author": author,
-          "layout": layout,
-          "type": type,
-          "tags": tags
-        });
-
-    },
-    'removeArticle': function(selectedArticle) {
-      ArticleList.remove(selectedArticle);
-    }
-  });
+        },
+        'removeArticle': function(selectedArticle) {
+            ArticleList.remove(selectedArticle);
+        }
+    });
 }
