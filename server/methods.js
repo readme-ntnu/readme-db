b439c8e36bba637a11422371f5c4bc4fc87510cd
Stein-Otto Svorstøl <steinotto.svorstol@gmail.com>
2016-01-24 17:15:26 +0100

    Remove isServer check as it is not needed when files are in `/server`-folder

--- server/methods.js
+++ server/methods.js
@@ -1,7 +1,20 @@
-if (Meteor.isServer) {
-    Meteor.methods({
-        'insertArticle': function(edition, url, pages, title, author, layout, type, tags) {
-            ArticleList.insert({
+Meteor.methods({
+    'insertArticle': function(edition, url, pages, title, author, layout, type, tags) {
+        ArticleList.insert({
+            "edition": edition,
+            "url": url,
+            "pages": pages,
+            "title": title,
+            "author": author,
+            "layout": layout,
+            "type": type,
+            "tags": tags
+        });
+    },
+    'updateArticle': function(selectedArticle, edition, url, pages, title, author, layout, type, tags) {
+        ArticleList.update(
+            {_id: selectedArticle},
+            {
                 "edition": edition,
                 "url": url,
                 "pages": pages,
@@ -11,24 +24,9 @@ if (Meteor.isServer) {
                 "type": type,
                 "tags": tags
             });
-        },
-        'updateArticle': function(selectedArticle, edition, url, pages, title, author, layout, type, tags) {
-            ArticleList.update(
-                {_id: selectedArticle},
-                {
-                    "edition": edition,
-                    "url": url,
-                    "pages": pages,
-                    "title": title,
-                    "author": author,
-                    "layout": layout,
-                    "type": type,
-                    "tags": tags
-                });
 
-        },
-        'removeArticle': function(selectedArticle) {
-            ArticleList.remove(selectedArticle);
-        }
-    });
-}
+    },
+    'removeArticle': function(selectedArticle) {
+        ArticleList.remove(selectedArticle);
+    }
+});
