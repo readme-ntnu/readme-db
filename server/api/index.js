319f87c99e4e37a7574ee703f54fdeb7cb297641
Mats Byrkjeland <matsbyr@gmail.com>
2017-02-17 22:09:51 +0100

    Create Slack integration endpoint

--- server/api/index.js
+++ server/api/index.js
@@ -1,5 +1,6 @@
 import { Restivus } from 'meteor/nimble:restivus';
 import search from './search';
+import slack from './slack';
 
 // Global API configuration
 const Api = new Restivus({
@@ -9,3 +10,6 @@ const Api = new Restivus({
 
 // Maps to: /api/search
 Api.addRoute('search', { authRequired: false }, search);
+
+// Maps to: /api/slack
+Api.addRoute('slack', { authRequired: false }, slack);
---
+++ server/api/slack/index.js
@@ -0,0 +1,38 @@
+import ArticleList from '../../../lib/ArticleList';
+import Helpers from '../../../lib/helpers/helpers';
+
+const slack = {
+  post() {
+    const query = this.bodyParams.text || '';
+    // Array of all space-separated keywords
+    const keywords = query.trim().split(' ');
+
+    // The url corresponding to this search on arkiv.abakus.no
+    const url = `http://arkiv.abakus.no/${query}`;
+
+    // Search for each keyword and intersect with resultArray to find articles present in all
+    const articles = keywords
+      .map(keyword => ArticleList.search(keyword.trim()).fetch())
+      .reduce(Helpers.intersect);
+
+    const body = {
+      text: `Søk etter *${query}* gir følgende artikler: ${url}`,
+      response_type: 'in_channel',
+      attachments: articles.slice(0, 10).map(a => (
+        {
+          title: a.title,
+          title_link: Helpers.getUrlFromEdition(a.edition, a.pages),
+          text: `Utgave ${a.edition} side ${a.pages}`,
+        })),
+    };
+
+    return {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body,
+    };
+  },
+};
+
+export default slack;
